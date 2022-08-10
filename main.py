931de5fe3bed420a6a8d14ac08f7c51d8b976c29
Waldo Panozo <waldo.panozo@gmail.com>
2022-08-10 08:15:45 -0400

    Starting in a new branch for maintain the dev zone out of the main zone LOL!

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 ###checking comments in python, say hello!!!
+from cmath import phase
+
+
 print("Hola como va!")
+
+## Variables
+# declaration of vars
+myVar = "Hey..."
+print(myVar)
+
+# using in different ways, beacuse python is not 
+# restrictive about the kind of type of var
+myVar = 34
+print(myVar)
+
+# declare 3 vars and concat the strings
+
+name = "Waldo"
+phone = 591888848484
+email = "waldo.panozo@gmail.com"
+
+print("Name: %s\nPhone: %d\nEmail: %s\n" % (name,phone,email))
+
---
+++ sum.py
@@ -0,0 +1,10 @@
+# Basics of aritmethics 
+a = 10
+b=23
+c = a+b
+# concat for print check later the use of %
+print("%d + %d = %d" % (a,b,c))
+
+# memory address of a var 
+# (only show the mem add for check where is located the var)
+print(id(c))
